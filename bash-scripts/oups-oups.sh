28fb2d2704f6ea54a31f29770ad7f3739bee97f1
gbesset <guillaume.besset.pro@gmail.com>
2021-01-22 11:26:22 +0100

    Fix bash script

--- bash-scripts/oups-oups.sh
+++ bash-scripts/oups-oups.sh
@@ -4,7 +4,7 @@ if [ -n "$(git status --porcelain)" ]; then
   echo "Il y a des changements en cours, commitez les d'abord ou supprimez les";
 else
   git remote add fork https://github.com/gbesset-formateur/2021-etu-iut-casir-git-tp4.git
-  git pull fork amend-bis
+  git pull fork fork/amend-bis
   git reset HEAD^
   git add src/index.html
   git commit -m 'Oh I miss the image'
